145958d3bf24c1b75273c11ae62ba5f42f32a942
hussainarthuna <hussainarthuna@gmail.com>
2020-11-26 14:04:18 +0300

    Navigation Service done

--- src/app/providers/navigation.service.ts
+++ src/app/providers/navigation.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NavigationService {
 
-  constructor() { }
+    constructor(
+        private nav: NavController,
+        private router: Router
+    ) {
+    }
+
+    goBack() {
+        this.nav.back();
+    }
+
+    go(path: string): void {
+        this.nav.navigateForward(path);
+    }
+
+    // Navigation tree will reset here.
+    goAndSetRoot(path: string): void {
+        this.nav.navigateRoot(path);
+    }
+
 }
